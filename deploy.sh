782dcdeb60e4f4c0093f07ec334ff0863201215b
Julian Baltuttis <julianbaltuttis@gmail.com>
2023-01-04 15:58:38 +0100

    Adds deploy script.

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ yarn run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
+
 git push -f git@github.com:julianbaltuttis/res-arcana-primer.git master:gh-pages
 cd -
