54b35e11ee15d5209a5cb2067682d5c62e837178
Julian Baltuttis <julianbaltuttis@gmail.com>
2023-01-05 11:24:40 +0100

    Sets production mode.

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,8 @@ router.beforeEach((to, from, next) => {
 })
 
 // VueLogger configuration and loading
-const isProduction = process.env.NODE_ENV === 'production';
-// const isProduction = true
+// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = true
 
 const options = {
   isEnabled: true,
